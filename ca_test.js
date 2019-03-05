50b74301d6814b986961c99e3971f4718f76a69d
Cakoluchiam <cakoluchiam+github@gmail.com>
2019-03-05 14:39:20 -0800

    Update ca_test.js

--- ca_test.js
+++ ca_test.js
@@ -1,104 +1,49 @@
-// SEE BELOW UNDER "GET STUFF" FOR ACTUAL CONTENT FETCH EXAMPLE
-document.getElementById('content').appendChild(document.createTextNode('Loading content…\n'));
-
-// Client ID and API key from the Developer Console
-var CLIENT_ID = '349213903572-kpj1jgjfqcumch94jo0gn07gfiu70c3c.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyCAFUy___xOoeW5Qb0g_Xu-eSGUeY4IxQc';
-
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
-
-var authorizeButton = document.getElementById('authorize_button');
-var signoutButton = document.getElementById('signout_button');
-
 /**
- *  On load, called to load the auth2 library and API client library.
+ *  Here's where we do stuff. Well, first some housekeeping to make sure we're signed in and can manipulate the sheets.
  */
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
+
+// Anything special when the user signs in. For now let's just initialize everything by default.
+// Called by sheets_auth.js once the user is signed in, and each time the user authorizes thereafter
+function sign_in() {
+  start();
 }
 
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES
-  }).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-  }, function(error) {
-    appendPre(JSON.stringify(error, null, 2));
-  });
-}
+// Anything special when the user signs out. Clean-up for reinitialization etc.
+// Called by sheets_auth.js whenever the user signs out
+function sign_out() {}
 
 /**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
+ *  Here's where we ACTUALLY do stuff.
  */
-function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    authorizeButton.style.display = 'none';
-    signoutButton.style.display = 'block';
-    listMajors();
-  } else {
-    authorizeButton.style.display = 'block';
-    signoutButton.style.display = 'none';
-  }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
-}
-
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-  var pre = document.getElementById('content');
-  var textContent = document.createTextNode(message + '\n');
-  pre.appendChild(textContent);
-}
-
-// THIS IS WHERE WE GET STUFF FROM THE SHEET.
-/**
- * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1k1mtGDWUmDmOh8qCV8CK5gcY6cIoKmG7KmS6hS-b49k/edit
- */
-function listMajors() {
-  gapi.client.sheets.spreadsheets.values.get({
+function start() {
+  console.log('Starting!');
+  appendPre('Started successfully! Stand by…\n');
+  var params, request, valueRangeBody;
+  
+  // TEST Cell Data GET
+  // See https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/get
+  params = {
+    // The ID of the spreadsheet to update.
     spreadsheetId: '1k1mtGDWUmDmOh8qCV8CK5gcY6cIoKmG7KmS6hS-b49k',
+
+    // The A1 notation of the values to update.
     range: 'API Test!B2:D',
-  }).then(function(response) {
+    
+    // How values should be represented in the output.
+    // The default render option is ValueRenderOption.FORMATTED_VALUE.
+    valueRenderOption: 'FORMATTED_VALUE',
+
+    // How dates, times, and durations should be represented in the output.
+    // This is ignored if value_render_option is
+    // FORMATTED_VALUE.
+    // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+    dateTimeRenderOption: 'FORMATTED_STRING',
+  };
+  request = gapi.client.sheets.spreadsheets.values.get(params);
+  request.then(function(response) {
     var range = response.result;
     if (range.values.length > 0) {
-      appendPre('Col.B2+, Col.D2+:');
+      appendPre('GET TEST\nCol.B2+, Col.D2+:');
       for (i = 0; i < range.values.length; i++) {
         var row = range.values[i];
         // Print columns B and D, which correspond to indices 0 and 2.
@@ -110,4 +55,50 @@ function listMajors() {
   }, function(response) {
     appendPre('Error: ' + response.result.error.message);
   });
+  
+  // Test Cell Data UPDATE
+  // See https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/update
+  params = {
+    // The ID of the spreadsheet to update.
+    spreadsheetId: '1k1mtGDWUmDmOh8qCV8CK5gcY6cIoKmG7KmS6hS-b49k',
+
+    // The A1 notation of the values to update.
+    range: 'API Test!F1',
+
+    // How the input data should be interpreted.
+    valueInputOption: 'USER_ENTERED',
+    
+    includeValuesInResponse: true,
+    
+    responseValueRenderOption: 'FORMATTED_VALUE',
+    
+    responseDateTimeRenderOption: 'FORMATTED_STRING',
+  };
+
+  var date = new Date();
+  valueRangeBody = {
+    "values": [[date.getFullYear()+'-'+date.getMonth()+'-'+date.getDate()+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds()]],
+  };
+
+  request = gapi.client.sheets.spreadsheets.values.update(params, valueRangeBody);
+  request.then(function(response) {
+    appendPre('\nUPDATE TEST');
+    appendPre('Last Accessed:');
+    appendPre(response.result.updatedData.values);
+  }, function(reason) {
+    appendPre('Error: ' + reason.result.error.message);
+  });
+  
+  /******** DOM INTERACTION STUFF ********/
+  /**
+   * Append a pre element to the body containing the given message
+   * as its text node. Used to display the results of the API call.
+   *
+   * @param {string} message Text to be placed in pre element.
+   */
+  function appendPre(message) {
+    var pre = document.getElementById('content');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+  }
 }
