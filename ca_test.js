01409dc9f9b4e117e6562e3dba3f40bef22032c0
Cakoluchiam <cakoluchiam+github@gmail.com>
2019-03-08 19:27:46 -0800

    simplified getters & setters

--- ca_test.js
+++ ca_test.js
@@ -12,6 +12,73 @@ function sign_in() {
 // Called by sheets_auth.js whenever the user signs out
 function sign_out() {}
 
+var DEFAULT_SHEET = '1k1mtGDWUmDmOh8qCV8CK5gcY6cIoKmG7KmS6hS-b49k';
+
+/**
+ * Simple get, returns range from default sheet as columns
+ *
+ * @param {string} A1 notation of range to get
+ * @param {function} callback function (optional; defaults to console.log)
+ * @param {function} error callback (optional; defaults to console.error)
+ * @param {params} any special parameters, using GSheets API (optional)
+ * @param {boolean} set TRUE if you want the CB called on the full response;
+ * .                defaults to CB called only with the value array (optional)
+ * .                (error is always called with full response)
+ */
+function get(range, callback=console.log, error=console.error, params={}, cb_full=false) {
+  var request;
+  params = Object.assign({
+    spreadsheetId: DEFAULT_SHEET,
+    range: range,
+    majorDimension: 'COLUMNS'
+  },params);
+  
+  request = gapi.client.sheets.spreadsheets.values.get(params);
+  request.then(function(response){
+    if(cb_full) callback(response);
+    else callback(response.result.values);
+  }, error);
+}
+
+
+/**
+ * Simple set, sets range to match an array of columns or a value
+ *
+ * @param {string} A1 notation of range to set
+ * @param {Array or value} Array of column data, e.g. [['A1','A2',...],['B1','B2',...],...]
+ * .                       if not passed an Array, will send value as [[value]]
+ * @param {function} callback function (optional; defaults to none)
+ * @param {function} error callback (optional; defaults to console.error)
+ * @param {Object} any special parameters, using GSheets API
+ * @param {Object} any special parameters to put into the values
+ * @param {boolean} set TRUE if you want the CB called on the full response;
+ * .                defaults to CB called only with the value array
+ * .                (error is always called with full response)
+ */
+function set(range, values, callback=function(){}, error=console.error, params={}, valueParams={}, cb_full=false) {
+  var request;
+  params = Object.assign({
+    spreadsheetId: DEFAULT_SHEET,
+    range: range,
+    valueInputOption: 'USER_ENTERED',
+    includeValuesInResponse: true
+  },params);
+  if(!Array.isArray(values)) values = [[values]];
+  valueParams = Object.assign({
+    values: values,
+    majorDimension: 'COLUMNS'
+  },valueParams);
+  
+  request = gapi.client.sheets.spreadsheets.values.update(params,valueParams);
+  request.then(function(response){
+    if(cb_full) callback(response);
+    else callback(response.result.updatedData.values);
+  }, error);
+}
+
+window.get = get;
+window.set = set;
+
 /**
  *  Here's where we ACTUALLY do stuff.
  */
